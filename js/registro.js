f204b7070a69a876fa399b90eb4d2c3e119cbe8c
TruenoxDDH <IIXdiazXII@gmail.com>
2022-10-12 23:16:22 -0500

    Siete commit

--- js/registro.js
+++ js/registro.js
@@ -18,5 +18,20 @@ function registro(e){
     if(nombreVal == ''|| emailVal == ''|| userVal ==''||passValue ==''){
         return;
     }
-    console.log('validando registro');
+    
+    const usuario = {
+        nombre: nombreVal,
+        email: emailVal,
+        user: userVal,
+        pass: passValue
+    }
+    localStorage.setItem('usuario',JSON.stringify(usuario));
+
+   nombre.value = '';
+   email.value = '';
+   user.value = '';
+   pass.value = '';
+    
+
+    console.log('usuario guardado');
 }
